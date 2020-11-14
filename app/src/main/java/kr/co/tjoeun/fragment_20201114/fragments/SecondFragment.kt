b0f548e529c6dda7072290a709cc8d84bc351bd2
조경진 <kj_cho@idealidea.co.kr>
2020-11-14 14:41:40 +0900

    두번째 프래그먼트 생성 (동작코드는 아직 X)

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20201114/fragments/SecondFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.fragment_20201114.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20201114.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+}
