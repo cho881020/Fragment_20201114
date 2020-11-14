caecb6662dfd47abb74007ff9d36fd8cdec6b78e
조경진 <kj_cho@idealidea.co.kr>
2020-11-14 14:46:44 +0900

    두번째 프래그먼트 - 버튼 누르면 얼럿띄우기 (아직 액티비티에 배치 X)

--- app/src/main/java/kr/co/tjoeun/fragment_20201114/fragments/SecondFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20201114/fragments/SecondFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
 import kr.co.tjoeun.fragment_20201114.R
 
 class SecondFragment : Fragment() {
@@ -15,6 +17,22 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        alertBtn.setOnClickListener {
+
+            val alert = AlertDialog.Builder(context!!)
+            alert.setTitle("프래그먼트 연습")
+            alert.setMessage("프래그먼트에서 얼럿을 띄웁니다.")
+            alert.setPositiveButton("확인", null)
+
+            alert.show()
+
+        }
+
     }
 
 }
